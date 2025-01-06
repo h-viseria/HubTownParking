49dedaca3158f1f107508490b531379bcd215fc8
h-viseria <hviseria@gmail.com>
2025-01-06 15:06:44 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,20 @@
-const CACHE_NAME = "parking-pwa-cache-v1";
-const urlsToCache = [
-  '/',
-  '/index.html'
-];
-
-// Install the service worker and cache files
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// Activate and clean up old caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
+    event.waitUntil(
+        caches.open('static-v1').then((cache) => {
+            return cache.addAll([
+                '/index.html',
+                '/style.css',
+                '/manifest.json',
+                '/icon/icon-192x192.png'
+            ]);
         })
-      );
-    })
-  );
+    );
+    console.log('Service Worker Installed');
+    self.skipWaiting();
 });
 
-// Fetch resources from cache and update when online
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
 });
